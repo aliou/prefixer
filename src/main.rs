4dabe186b0b0b0a10f3cd147f7ec5ec9ce7d0bba
Aliou Diallo <code@aliou.me>
2018-08-26 18:48:59 +0200

    Initial implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::BufRead;
+
+const DEFAULT_PREFIX_CHARACTER: &'static str = "#";
+
+fn main() -> io::Result<()> {
+    // Get the standard input shared handle.
+    let stdin = io::stdin();
+
+    // Lock the handle so we can read from it.
+    let handle = stdin.lock();
+
+    // Loop on the lines from the handle using the `BufRead` trait's `lines`.
+    for line in handle.lines() {
+        match line {
+            Ok(string) => println!("{}  {}", DEFAULT_PREFIX_CHARACTER, string),
+            _ => (),
+        }
+    }
+
+    Ok(())
 }
