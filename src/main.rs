5d393f834bed6ecfbbc24c3b423f9988a47568d4
Aliou Diallo <code@aliou.me>
2018-08-26 19:12:56 +0200

    Fix variable name.

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@ use std::io;
 use std::io::BufRead;
 
 // It seems like it's not possible to do something like:
-//   const DEFAULT_PREFIX_CHARACTER: &'static String = &String::from("#");
+//   const DEFAULT_PREFIX: &'static String = &String::from("#");
 // See `rustc --explain E0015`.
-const DEFAULT_PREFIX_CHARACTER: &'static str = "#";
+const DEFAULT_PREFIX: &'static str = "#";
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    // So... `character` is not the same type depending on the branch it goes through,
-    // but it works because I don't explicitly define the character type.
-    let character = if args.len() > 1 {
+    // So... `prefix` is not the same type depending on the branch it goes through,
+    // but it works because I don't explicitly define the variable type.
+    let prefix = if args.len() > 1 {
         &args[1]
     } else {
-        DEFAULT_PREFIX_CHARACTER
+        DEFAULT_PREFIX
     };
 
     // Get the standard input shared handle.
@@ -27,7 +27,7 @@ fn main() -> io::Result<()> {
     // Loop on the lines from the handle using the `BufRead` trait's `lines`.
     for line in handle.lines() {
         match line {
-            Ok(string) => println!("{} {}", character, string),
+            Ok(string) => println!("{} {}", prefix, string),
             _ => (),
         }
     }
